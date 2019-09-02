aae9b164eb4efa6e8fd202ad07204fe0556bff7d
Gabriela Moraes <gabrielademoraes10@gmail.com>
2019-09-01 22:42:48 -0300

    Montando desenho inicial do grafo.
    Co-authored-by: LucasLermen <lucas.arthur.lermen@gmail.com>

--- graph.py
+++ graph.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import json
+import matplotlib.pyplot as plt
+from networkx import nx
 from graphFunction import *
 
 def main():
@@ -14,22 +16,22 @@ def main():
     .dropna()
     df.to_json('metroSP.json')
 
+    with open('metroSPNotEdited.json') as json_file:
+        dataNotEdited = json.load(json_file)
+
     with open('metroSP.json') as json_file:
         data = json.load(json_file)
 
     listaDeAdjacencia = {}
     listaEstacoes = []
 
-        # Montando lista de adjacência
+    # Montando lista de adjacência
     for (key, estacao) in data['station'].items():
         if not listaDeAdjacencia.get(estacao):
             listaDeAdjacencia[estacao] = []
             listaEstacoes.append(estacao)
 
-
         listaDeAdjacencia[estacao].append(data['onlyNeigh'][key])
-
-    listaAeroportos = sorted((set(listaEstacoes)))
 
     # Salvando lista de adjacência
     with open('listaAdjacencia.json', 'w') as json_file:
@@ -37,4 +39,46 @@ def main():
     
     # print(listaDeAdjacencia)
     print(BFS(listaDeAdjacencia, 'campo-limpo', 'largo-treze'))
+
+    # print(dataNotEdited['name'].get('0'))
+
+    G = nx.Graph()
+    labels = {}
+    mapaDeCores = []
+
+    for estacao, arestas in listaDeAdjacencia.items():  
+
+        corNo = recupera_linha(dataNotEdited, estacao)
+        labels[estacao] = recupera_nome(dataNotEdited, estacao)  
+        G.add_node(estacao)  
+        # corNo = nx.get_node_attributes(G,'color')
+        # corNo = recupera_linha(dataNotEdited, estacao)
+        if corNo == '[lilas]':
+            mapaDeCores.append('#8B008B')
+        elif corNo == '[verde]':
+            mapaDeCores.append('#006400')
+        elif corNo == '[azul]':
+            
+            mapaDeCores.append('#000080')
+        elif corNo == '[vermelha]':
+            mapaDeCores.append('#FF0000')
+        elif corNo == '[amarela]':
+            mapaDeCores.append('#FFFF00')
+        elif corNo == '[prata]':
+            mapaDeCores.append('#C0C0C0')
+        else:
+            mapaDeCores.append('#FFDAB9')
+
+        for aresta in arestas:
+            G.add_edge(estacao, aresta)
+
+    print(mapaDeCores)
+    
+    listaNos = G.nodes()
+    listaNos = sorted((set(listaNos)))
+    posicoes = get_posicoes()
+
+    nx.draw(G, pos=posicoes, nodelist=listaNos,with_labels=True,labels=labels, node_color=mapaDeCores)
+    plt.show()
+
 main()
--- graphFunction.py
+++ graphFunction.py
@@ -1,3 +1,5 @@
+import json
+
 def BFS(grafo, estacaoOrigem, estacaoDestino):
     explorados = []
     fila = [[estacaoOrigem]]
@@ -31,5 +33,98 @@ def BFS(grafo, estacaoOrigem, estacaoDestino):
                     return novo_caminho
         explorados.append(no)
     return "Não existe caminho"
-    
+
+def recupera_nome(json_object, station):
+    for dicionario in json_object:
+        if station == dicionario['station']:
+            return dicionario['name']
+
+def recupera_linha(json_object, station):
+    for dicionario in json_object:
+        if station == dicionario['station']:
+            return dicionario['line']
+
+def get_posicoes():
+    pos = {
+        'aacd-servidor':(100,20),
+        'adolfo-pinheiro':(20,20),
+        'alto-da-boa-vista':(20,20),
+        'alto-do-ipiranga':(20,20),
+        'ana-rosa':(50,40),  #azul
+        'anhangabau':(20,20),
+        'armenia':(50, 76),  #azul
+        'artur-alvim':(20,20),
+        'belem':(20,20),
+        'borba-gato':(20,20),
+        'bras':(20,20),
+        'bresser-mooca':(20,20),
+        'brigadeiro':(20,20),
+        'brooklin':(20,20),
+        'butanta':(20,20),
+        'camilo-haddad':(20,20),
+        'campo-limpo':(20,20),
+        'capao-redondo':(20,20),
+        'carandiru':(50,84), #azul
+        'carrao':(20,20),
+        'chacara-klabin':(20,20),
+        'clinicas':(20,20),
+        'conceicao':(50,16),  #azul
+        'consolacao':(20,20),
+        'corinthians-itaquera':(20,20),
+        'eucaliptos':(20,20),
+        'faria-lima':(20,20),
+        'fradique-coutinho':(20,20),
+        'giovanni-gronchi':(20,20),
+        'guilhermina-esperanca':(20,20),
+        'higienopolis-mackenzie':(20,20),
+        'hospital-sao-paulo':(20,20),
+        'jabaquara':(50,12), #azul
+        'japao-liberdade':(50, 56), #azul
+        'jardim-sao-paulo-ayrton-senna':(50, 92), #azul
+        'largo-treze':(20,20),
+        'luz':(50,68),  #azul
+        'marechal-deodoro':(20,20),
+        'moema':(20,20),
+        'oratorio':(20,20),
+        'oscar-freire':(20,20),
+        'palmeiras-barra-funda':(20,20),
+        'parada-inglesa':(50, 96), #azul
+        'paraiso':(50,44),  #azul
+        'patriarca-vila-re':(20,20),
+        'paulista':(20,20),
+        'pedro-ii':(20,20),
+        'penha':(20,20),
+        'pinheiros':(20,20),
+        'portuguesa-tiete':(50,80),  #azul
+        'praca-da-arvore':(50,28),  #azul
+        'republica':(20,20),
+        'sacoma':(20,20),
+        'santa-cecilia':(20,20),
+        'santa-cruz':(50,32),  #azul
+        'santana':(50,88),  #azul
+        'santo-amaro':(20,20),
+        'santos-imigrantes':(20,20),
+        'santuario-nossa-senhora-de-fatima-sumare':(20,20),
+        'sao-bento':(50,64),  #azul
+        'sao-joaquim':(50, 52), #azul
+        'sao-judas':(50,20),  #azul
+        'sao-lucas':(20,20),
+        'sao-paulo-morumbi':(20,20),
+        'saude':(50,24),  #azul
+        'se':(50,60),  #azul
+        'tamanduatei':(20,20),
+        'tatuape':(20,20),
+        'tiradentes':(50,72),  #azul
+        'trianon-masp':(20,20),
+        'tucuruvi':(50,100), #azul
+        'vergueiro':(50,48),  #azul
+        'vila-das-belezas':(20,20),
+        'vila-madalena':(20,20),
+        'vila-mariana':(50,36),  #azul
+        'vila-matilde':(20,20),
+        'vila-prudente':(20,20),
+        'vila-tolstoi':(20,20),
+        'vila-uniao':(20,20) }
+    return pos
+
 
